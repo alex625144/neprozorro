9941ad2a1b0dd0086ff2325874c755a25856b4c0
alex625144 <benechka1@gmail.com>
2023-03-02 20:55:00 +0200

    created project

---
+++ src/main/java/com/neprozorro/NeprozorroApplication.java
@@ -0,0 +1,12 @@
+package main.java.com.neprozorro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeprozorroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeprozorroApplication.class, args);
+    }
+}
