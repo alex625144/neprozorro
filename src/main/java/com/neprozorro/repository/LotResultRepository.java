730dcbf80a18673aa1286e8d7f898b05800188b5
alex625144 <benechka1@gmail.com>
2023-03-07 23:16:33 +0200

    fix

--- src/main/java/com/neprozorro/repository/LotResultRepository.java
+++ src/main/java/com/neprozorro/repository/LotResultRepository.java
@@ -2,7 +2,6 @@ package com.neprozorro.repository;
 
 import com.neprozorro.model.LotPDFResult;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.UUID;
 
 public interface LotResultRepository extends JpaRepository<LotPDFResult, UUID> {
