73cb005a303f8be8845fecd3ac01253aa3e452a1
alex625144 <benechka1@gmail.com>
2023-03-25 13:46:11 +0200

    create classes

--- src/main/java/com/neprozorro/rest/ResultResource.java
+++ src/main/java/com/neprozorro/rest/ResultResource.java
@@ -3,6 +3,7 @@ package com.neprozorro.rest;
 import com.neprozorro.model.ResultReport;
 import com.neprozorro.service.ResultService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,8 @@ public class ResultResource {
 
     @GetMapping("/laptops/dateRange?from={startDate},to={endDate}")
     @ResponseStatus(HttpStatus.FOUND)
-    public List<ResultReport> getResultTable(@PathVariable LocalDate startDate, @PathVariable LocalDate endDate) {
+    public List<ResultReport> getResultTable(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+                                             @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
         List<ResultReport> resultReports = new ArrayList<>();
         try {
             resultReports =resultService.getResult(startDate, endDate);
